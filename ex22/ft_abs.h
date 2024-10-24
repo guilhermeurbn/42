241f11b7a185dae80c4b74440d7b6a655585401b
Guilherme Urbano <guilhermeurbano@MacBookPro.lan>
2024-10-24 19:22:20 +0100

    :tada:

--- ex22/ft_abs.h
+++ ex22/ft_abs.h
@@ -6,7 +6,8 @@
 /*   By: guilhermeurbano <guilhermeurbano@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 19:12:39 by guilhermeur       #+#    #+#             */
-/*   Updated: 2024/10/24 19:12:40 by guilhermeur      ###   ########.fr       */
+/*   Updated: 2024/10/24 19:19:34 by guilhermeur      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void;
